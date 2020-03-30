6e6c43dd87f4e6ceda086d81297ec731270bd38f
mai cook <mailiecook@yahoo.com>
2020-03-30 16:30:21 -0700

    debug scripts2.js

--- js/scripts2.js
+++ js/scripts2.js
@@ -3,15 +3,19 @@ $(document).ready(function(){
     event.preventDefault();
     var sentence = $("input#sentence").val();
     var sentenceArray = sentence.split(" ");
+
+    // instantiate a new empty array 
     console.log(sentenceArray);
 
     sentenceArray.map(function(word){
       // var index = [1,2,3];
-      var sort = $("input#sentence"+word.length(3)); 
+      var sort = $("input#sentence"+ word).length(3)); 
          console.log(sort);
-      
-     
     });
+
+    // forEach loop through sentenceArray
+      // use simple branching to see if the word.length is greater than 2
+        // push to the new array
 
   });
 });
