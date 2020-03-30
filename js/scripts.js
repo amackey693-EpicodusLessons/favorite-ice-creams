ba60bd35f47110803c1faea59ec1a344a0858354
mai cook <mailiecook@yahoo.com>
2020-03-30 14:34:08 -0700

    create index2.html, styles2.css, scripts2.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,10 @@ $(document).ready(function(){
     flavorArray.forEach(function(flavor){
       var userInput = $("#flav"+flavor).val();
       $(".answer").append(" " + userInput); 
-      // console.log(index);
-      // console.log(flavor);
-      // console.log($(flavor));
-      // console.log(userInput);
-
-    });
     
+
+    }); 
 
   });
 
 });
-
-
-// $(document).ready(function() {
-//   $("#formOne").submit(function(event) {
-//     event.preventDefault();
-//     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
-
-//     console.log(blanks)
-
-//     blanks.forEach(function(blank) {
-//       var userInput = $("input#" + blank).val();
-//       $("." + blank).text(userInput);
-//     });
-
-//     $("#story").show();
-
-    
-//   });
-// });
