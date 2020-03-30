9e0700098d2e0210617310f52c004ba905a9c550
mai cook <mailiecook@yahoo.com>
2020-03-30 16:24:20 -0700

    debug scripts2.js

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var sentence =("#se")
-    var sentenceArray = [] 
+    var sentence = $("input#sentence").val();
+    var sentenceArray = sentence.split(" ");
+    console.log(sentenceArray);
 
+    sentenceArray.map(function(word){
+      // var index = [1,2,3];
+      var sort = $("input#sentence"+word.length(3)); 
+         console.log(sort);
+      
+     
+    });
 
   });
-  
 });
