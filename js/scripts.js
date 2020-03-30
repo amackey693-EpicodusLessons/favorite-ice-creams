e8033fcf9ea4961f814e3777d73296209d588a41
mai cook <mailiecook@yahoo.com>
2020-03-30 14:27:47 -0700

    finalizes flavor arrays & looping for our favorite ice cream flavors

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,40 @@
 $(document).ready(function(){
-  $("#form").submit(function(event){
+  $("form#flavorInput").submit(function(event){
     event.preventDefault();
+    var flavorArray = [1,2,3];
 
-    var favorite = ['#', '#', '#']
+    // console.log(flavorA);
+
+    flavorArray.forEach(function(flavor){
+      var userInput = $("#flav"+flavor).val();
+      $(".answer").append(" " + userInput); 
+      // console.log(index);
+      // console.log(flavor);
+      // console.log($(flavor));
+      // console.log(userInput);
+
+    });
+    
 
   });
 
 });
+
+
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     event.preventDefault();
+//     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+//     console.log(blanks)
+
+//     blanks.forEach(function(blank) {
+//       var userInput = $("input#" + blank).val();
+//       $("." + blank).text(userInput);
+//     });
+
+//     $("#story").show();
+
+    
+//   });
+// });
